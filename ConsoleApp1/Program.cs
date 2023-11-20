95ae74c7d350bf715481de7c01ba81147afb3bef
Khalel Albuquerque <khaleledipo@gmail.com>
2023-11-20 18:33:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite três números:");
+            Console.Write("Número 1: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Número 2: ");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.Write("Número 3: ");
+            int n3 = int.Parse(Console.ReadLine());
+
+            double result = Maior(n1, n2, n3);
+            Console.WriteLine(result);
+        }
+
+        static int Maior(int a, int b, int c)
+        {
+            int m;
+            if(a>b && a > c)
+            {
+                m = a;
+            }else if(b > a && b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+
+            return m;
+        }
+    }
+}
