2165085b86179e4053ba4356b99927140660982e
Khalel Albuquerque <khaleledipo@gmail.com>
2023-11-20 23:51:19 -0300

    encapsulation / properties

---
+++ Encapsulation/Produto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Produto
+    {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Qtd { get; private set; }
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco, int qtd)
+        {
+            _nome = nome;
+            Preco = preco;
+            Qtd = qtd;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+
+            }
+        }
+
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Qtd;
+        }
+
+        public void AddQtd(int qtdAdd)
+        {
+            this.Qtd += qtdAdd;
+        }
+
+        public void RemQtd(int qtdRem)
+        {
+            this.Qtd -= qtdRem;
+        }
+
+        public override string ToString()
+        {
+            return _nome + ", $ " + Preco + ", Qtd: " + Qtd + ", Total: $ " + this.ValorTotalEmEstoque();
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto("TV", 200, 10);
+
+            Console.WriteLine(p.Nome);
+            p.Nome = "Kasda";
+            Console.WriteLine(p.Nome);
+
+        }
+    }
+}
