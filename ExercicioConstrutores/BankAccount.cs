02e2ecc0428551ec90ba06c64a3b766a1b8c4d24
Khalel Albuquerque <khaleledipo@gmail.com>
2023-11-24 23:26:52 -0300

    Unfinished constructor exercise

---
+++ ExercicioConstrutores/BankAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioConstrutores
+{
+    internal class BankAccount
+    {
+        private string _accountNumber;
+        private string _accountOwner;
+        public double Value { get; private set; }
+
+        public BankAccount(string accountNumb, string accountOwn)
+        {
+            this._accountNumber = accountNumb;
+            this._accountOwner = accountOwn;
+            this.Value = 0;
+        }
+        public BankAccount(string accountNumb, string accountOwn, double value)
+        {
+            this._accountNumber = accountNumb;
+            this._accountOwner = accountOwn;
+            this.Value = value;
+        }
+
+        public string AccountNumber
+        {
+            get { return _accountNumber; }
+        }
+
+        public string AccountOwner
+        {
+            get => _accountOwner;
+            set
+            {
+                if(_accountOwner != null)
+                {
+                    _accountOwner = value;
+                }
+            }
+        }
+
+        public void addValue(double value)
+        {
+            this.Value += value;
+        }
+
+        public void removeValue(double value)
+        {
+            if(this.Value < value) {
+                Console.WriteLine("Valor insuficiente");
+            }
+            else
+            {
+                this.Value -= (value+5);
+            }
+        }
+
+
+
+
+
+        public override string ToString()
+        {
+            return "Conta " + _accountNumber + ", Titular: " + _accountOwner + ", Saldo: " + Value;
+        }
+    }
+}
---
+++ ExercicioConstrutores/Program.cs
@@ -0,0 +1,44 @@
+﻿using ExercicioConstrutores;
+using System; 
+
+namespace ConstructorExercise
+{
+    class ConstructorExercise
+    {
+        static void Main(string[] args)
+        {
+            string accountNumber;
+            string accountOwner;
+            char optionDeposit;
+            double initialValue;
+
+            Console.Write("Digite o número da conta: ");
+            accountNumber = Console.ReadLine();
+
+            Console.Write("Digite o titular da conta: ");
+            accountOwner = Console.ReadLine();
+
+            Console.Write("Deseja fazer depósito inicil? (s/n): ");
+            optionDeposit = char.Parse(Console.ReadLine());
+
+
+            Console.Write("Digite o valor de depósito: ");
+            initialValue = double.Parse(Console.ReadLine());
+
+            BankAccount userAccount = new BankAccount(accountNumber, accountOwner, initialValue);
+
+            Console.Write(userAccount.ToString());
+
+            Console.Write("Digite o valor de saque: ");
+            userAccount.removeValue(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Novo valor: " + userAccount.Value);
+
+            Console.Write("Digite o valor de deposito: ");
+            userAccount.addValue(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Novo valor: " + userAccount.Value);
+
+        }
+    }
+}
