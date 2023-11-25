bffec55847f3852a595b0d130c3cfa8ea321666f
Khalel Albuquerque <khaleledipo@gmail.com>
2023-11-25 14:37:52 -0300

    Matrix numbers around

---
+++ MatrixAroundNumber/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace MatrixAround
+{
+    class MatrixAround
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite o primeiro tamanho da matriz: ");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo tamanho da matriz: ");
+            int second = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[first, second];
+
+            for(int i=0; i<matrix.GetLength(0); i++)
+            {
+                for(int j=0; j<matrix.GetLength(1); j++)
+                {
+                    Console.Write("Digite o numero [" + i + ", " + j + "] da matriz: ");
+                    int number = int.Parse(Console.ReadLine());
+                    matrix[i,j] = number;
+                }
+            }
+
+            Console.WriteLine("");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string linha = "";
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    linha += (matrix[i, j] + " ");
+                }
+                Console.WriteLine(linha);
+            }
+
+            void PrintarArredor(int x, int y)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ponto " + x + ", " + y + ":");
+
+                //Esquerda
+                if (y - 1 >= 0)
+                    Console.WriteLine("Esquerda: " + matrix[x, y - 1]);
+                //Direita
+                if (y + 1 < matrix.GetLength(1))
+                    Console.WriteLine("Direita: " + matrix[x, y + 1]);
+                //Up
+                if (x - 1 >= 0)
+                    Console.WriteLine("Cima: " + matrix[x - 1, y]);
+                //Down
+                if (x + 1 < matrix.GetLength(0))
+                    Console.WriteLine("Baixo: " + matrix[x + 1, y]);
+
+
+            }
+
+            Console.WriteLine();
+            Console.Write("Digite o numero que quer comparar: ");
+            int sorteado = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(matrix[i, j] == sorteado)
+                    {
+                        PrintarArredor(i, j);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MatrixExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace MatrixExercise
+{
+    class MatrixExercise
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o tamanho da matriz: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            int innerCounter = 0;
+            int[,] mat = new int[tamanho, tamanho];
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    Console.Write("Digite o numero [" + i + ", " + j + "] da matriz: ");
+                    int num = int.Parse(Console.ReadLine());
+                    if (num < 0)
+                        innerCounter++;
+                    mat[i, j] = num;
+                }
+            }
+
+            List<int> diagonal = new List<int>();
+
+
+            Console.WriteLine("Diagonal principal: ");
+            for (int i = 0;i < mat.GetLength(0); i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            Console.WriteLine("");
+            Console.Write("Numeros negativos: " + innerCounter);
+
+
+
+        }
+    }
+}
